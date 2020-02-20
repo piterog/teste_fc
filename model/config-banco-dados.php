403d840d3124010e86b4eec07d8f6acf8154bb4a
Piter Ortiz <piter_og@hotmail.com>
2020-02-20 17:55:54 -0300

    Conexao com BD

---
+++ model/config-banco-dados.php
@@ -0,0 +1,14 @@
+<?php
+class Database
+{
+    public static function initDatabase()
+    {
+        try {
+            $pdo = new PDO('mysql:host=127.0.0.1;dbname=teste_fc;charset=utf8', 'root', '123123');
+            $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
+            return $pdo;
+          } catch(PDOException $e) {
+              die("Falha na conexão: " . $e->getMessage());
+          }
+    }
+}
