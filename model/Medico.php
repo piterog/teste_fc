fc37fad21bc675ada230ae3481f689c5b38922b8
Piter Ortiz <piter_og@hotmail.com>
2020-02-20 20:33:52 -0300

    Medico model finished

--- model/Medico.php
+++ model/Medico.php
@@ -32,8 +32,53 @@ class Medico
 	{
 		try{
 			$stm = $this->pdo->prepare("SELECT * FROM ".$this->table." WHERE id = ?");
-			$stm->execute(array($id));
+			$stm->execute([$id]);
 			return $stm->fetch(PDO::FETCH_OBJ);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+    }
+
+    public function store(Medico $data)
+	{
+        $now = date("Y-m-d H:i:s");
+
+		try {
+		    $sql = "INSERT INTO ".$this->table." (email,nome,senha,endereco_consultorio,data_criacao,data_alteracao) VALUES (?, ?, ?, ?, ?, ?)";
+
+            $this->pdo->prepare($sql)->execute([
+                            $data->email, $data->nome, $data->senha, 
+                            $data->endereco_consultorio, $now, $now
+                        ]);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+    }
+    
+    public function update($data)
+	{
+        $now = date("Y-m-d H:i:s");
+
+		try {
+			$sql = "UPDATE ".$this->table." SET email = ?,nome = ?,senha = ?,endereco_consultorio = ?,data_alteracao = ?					
+				    WHERE id = ?";
+
+			$this->pdo->prepare($sql)->execute([
+				    	$data->email, $data->nome, $data->senha, 
+                        $data->endereco_consultorio, $now,
+                        $data->id
+                    ]);
+		} catch (Exception $e) {
+			die($e->getMessage());
+		}
+	}
+    
+    public function delete($id)
+	{
+		try {
+			$stm = $this->pdo->prepare("DELETE FROM ".$this->table." WHERE id = ?");			          
+
+			$stm->execute([$id]);
 		} catch (Exception $e) {
 			die($e->getMessage());
 		}
--- model/config-banco-dados.php
+++ model/config-banco-dados.php
@@ -7,8 +7,8 @@ class Database
             $pdo = new PDO('mysql:host=127.0.0.1;dbname=teste_fc;charset=utf8', 'root', '123123');
             $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);	
             return $pdo;
-          } catch(PDOException $e) {
-              die("Falha na conexão: " . $e->getMessage());
-          }
+        } catch(PDOException $e) {
+            die("Falha na conexão: " . $e->getMessage());
+        }
     }
 }
